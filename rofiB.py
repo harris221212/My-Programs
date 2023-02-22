fbdab78139843bf1029a2a5ffa3357cde37f1103
ethang <esg2@st-andrews.ac.uk>
2023-02-22 12:46:39 +0000

    added delete to bookmarking

--- rofiB.py
+++ rofiB.py
@@ -11,9 +11,17 @@ bookmarks = f.readlines()
 f.close()
 
 for i,x in enumerate(bookmarks):
-	bookmarks[i] = x.strip()	
+	bookmarks[i] = x.strip()
 r = Rofi()
-index, key = r.select("Select Bookmark To Copy To Clipboard", bookmarks)
-pyperclip3.copy(bookmarks[index])
+index, key = r.select("Select Bookmark To Copy To Clipboard", bookmarks, key5=("Alt+w", "Delete"))
+if key == 5:
+	del bookmarks[index]
+	f = open(fileName, "w")
+	for x in bookmarks:
+		f.write(x)
+		f.write("\n")
+	f.close()
+elif key == 0:
+	pyperclip3.copy(bookmarks[index])
 #pyperclip.paste()
 
